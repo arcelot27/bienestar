02b97c08d9ab7a21990dbaea0efe9e7820462097
arcelot27 <artamua@gmail.com>
2024-05-21 15:21:33 -0500

    mvc actualización de interfaces

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -2,16 +2,27 @@ document.addEventListener("DOMContentLoaded", function() {
     const profileForm = document.getElementById("profileForm");
     const profileCard = document.querySelector('.profile-card');
     const profileLink = document.getElementById('profileLink');
-    const profileToggleIcon = document.querySelector('.fa-pen-to-square');
+    const profileToggleIcon = document.getElementById('toggleProfileForm');
 
-    if (profileForm) {
+    console.log("DOMContentLoaded event fired");
+
+    if (profileForm && profileCard && profileLink && profileToggleIcon) {
         profileLink.addEventListener("click", function(event) {
+            console.log("profileLink click event fired");
             event.preventDefault();  // Prevenir la redirección
             profileCard.classList.remove('hidden');
         });
 
         profileToggleIcon.addEventListener("click", function() {
+            console.log("profileToggleIcon click event fired");
             profileCard.classList.toggle('hidden');
+        });
+
+        window.addEventListener("click", function(event) {
+            console.log("window click event fired");
+            if (event.target === profileCard) {
+                profileCard.classList.add('hidden');
+            }
         });
     }
 });
--- view/user/enfermeria/enfermeria.php
+++ view/user/enfermeria/enfermeria.php
@@ -1,31 +1,31 @@
 <main>
-        <div class="main_header">
-            <a href="#" id="profileLink"><i class="fa-solid fa-user-nurse"></i></a>
-            <p><?php echo isset($_SESSION['name']) ? $_SESSION['name'] : ''; ?></p>
+    <div class="main_header">
+        <button id="profileLink"><i class="fa-solid fa-user-nurse"></i></button>
+        <p><?php echo isset($_SESSION['name']) ? $_SESSION['name'] : ''; ?></p>
+    </div>
+    <div class="main_body">
+        <div>
+            <a href="#">
+                <div>
+                    <i class="fa-solid fa-plus"></i>
+                </div>
+                <p>Realizar Tamizaje</p>
+            </a>
+            <a href="#">
+                <div>
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                </div>
+                <p>Consultar Tamizaje</p>
+            </a>
         </div>
-        <div class="main_body">
-            <div>
-                <a href="#">
-                    <div>
-                        <i class="fa-solid fa-plus"></i>
-                    </div>
-                    <p>Realizar Tamizaje</p>
-                </a>
-                <a href="#">
-                    <div>
-                        <i class="fa-solid fa-magnifying-glass"></i>
-                    </div>
-                    <p>Consultar Tamizaje</p>
-                </a>
-            </div>
-            <div>
-                <img src="assets/img/logo-4remove.png" alt="">
-            </div>
+        <div>
+            <img src="assets/img/logo-4remove.png" alt="">
         </div>
-    </main>
+    </div>
+</main>
 
-    <div class="profile-card hidden">
-  
+<div class="profile-card hidden">
+    <div>
         <div class="profile-header">
             <i class="fa-solid fa-user-nurse"></i>
             <p>Salud</p>
@@ -45,6 +45,7 @@
                 <button type="submit">Guardar</button>
             </form>
         </div>
-    </div>
+    </div>  
+</div>
 
-    <script src="../../../assets/js/profile.js>
+<script src="assets/js/profile.js"></script>
