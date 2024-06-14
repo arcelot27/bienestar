4ae6e6368f8cc0bcdf5e4de61b550f6bff6e56a0
Yeison Castellanos <86814629+yachostdev@users.noreply.github.com>
2024-06-14 17:52:53 -0500

    Upgrade_Views

--- view/heder_user.php
+++ view/heder_user.php
@@ -1,5 +1,5 @@
 <body>
     <header>
         <img src="assets/img/logo-5remov.png" alt="">
-        <a href="?b=admin&s=sessionexit" onclick="return confirm('¿Estás seguro de que deseas cerrar la sesión?')" style="margin-right: 10px;"><i class="fa-solid fa-arrow-right-from-bracket"></i></a>
+        <a id="logoutButton"><i class="fa-solid fa-arrow-right-from-bracket"></i></a>
     </header> 
